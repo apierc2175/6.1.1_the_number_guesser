1c1170852886e4e08c156e587464723b0f9d6dbc
apierc2175 <aidanwpierce@gmail.com>
2019-09-30 15:12:29 -0400

    all done and optimized

--- level_one.py
+++ level_one.py
@@ -1,13 +1,16 @@
 import random
 #prints 1 random number between 1 and 100
 rand_num = random.randint(1,101)
-print(rand_num)
 
 for i in range(5):
-    guess = input("guess a number between 1 and 100")
+    guess = input("guess a number between 1 and 100: ")
     if(guess == rand_num):
         print("You guessed correct!")
+        break
     elif(guess > rand_num):
         print("Your guess is too high!")
     elif(guess < rand_num):
         print("Your guess is too low")
+
+    if i == 4:
+        print("You are out of guesses you lose")
--- level_two.py
+++ level_two.py
@@ -1,13 +1,17 @@
 import random
 #prints 1 random number between 1 and 100
-user_number = input("choose a number between 1 and 100")
+user_number = input("choose a number between 1 and 100: ")
 print(user_number)
 
 for i in range(5):
     computer_num = random.randint(1,101)
     if(user_number == computer_num):
         print("The computer guessed correct! AI is smarter than people the robots will take over")
+        break
     elif(user_number > computer_num):
         print("the computer guess is too high!")
     elif(user_number < computer_num):
         print("the computer guess is too low")
+
+        if(i == 4):
+            print("the computer is out of guesses, win for humanity!")
